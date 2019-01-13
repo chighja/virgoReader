5b4290bc20230451150cbfb2d29e2b92920912e0
chighja <chighja@gmail.com>
2019-01-13 13:00:24 -0500

    started auth branch

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,21 @@
 import React from 'react';
+import auth0 from 'auth0-js';
+
+class Auth {
+  auth0 = new auth0.WebAuth({
+    domain: 'chighja.auth0.com',
+    clientID: 'xZzkAR7iQR77Ds0b23dnywzfEdQna0le',
+    redirectUri: 'http://localhost:3000/callback',
+    responseType: 'token id_token',
+    scope: 'openid'
+  });
+
+  login() {
+    this.auth0.authorize();
+  }
+}
+
+const auth = new Auth();
 
 function LoginContents() {
   return (
@@ -10,6 +27,10 @@ function LoginContents() {
 }
 
 class Login extends React.Component {
+  componentDidMount() {
+    auth.login();
+  }
+
   render() {
     return <LoginContents />;
   }
