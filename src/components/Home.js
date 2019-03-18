2bf209bebc3ecc931f66f3e62f31060efe53aab8
chighja <chighja@gmail.com>
2019-03-18 12:16:17 -0400

    installed oauth

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function Home() {
-  return (
-    <div>
-      <h3>I am the home page</h3>
-      <p>You are home</p>
-    </div>
-  );
+class Home extends Component {
+  login() {
+    this.props.auth.login();
+  }
+  render() {
+    const { isAuthenticated } = this.props.auth;
+    return (
+      <div className="container">
+        {isAuthenticated() && <h4>You are logged in!</h4>}
+        {!isAuthenticated() && (
+          <h4>
+            You are not logged in! Please{' '}
+            <a style={{ cursor: 'pointer' }} onClick={this.login.bind(this)}>
+              Log In
+            </a>{' '}
+            to continue.
+          </h4>
+        )}
+      </div>
+    );
+  }
 }
 
 export default Home;
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import Auth from '../Auth/Auth.js';
 
 function LoginContents() {
+  function engageLogin(e) {
+    e.preventDefault();
+    const auth = new Auth();
+    auth.login();
+  }
+
   return (
     <div>
       <h3>Login</h3>
       <p>Just login already</p>
+      <button onClick={engageLogin()}>Login</button>
     </div>
   );
 }
